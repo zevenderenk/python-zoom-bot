68156a8f6fe09dda7ebe85f5cb5ebe9d09909c98
zevenderenk <celikmertkayra266@gmail.com>
2024-07-03 20:08:57 +0300

    ClassStart.py

--- ClassStart.py
+++ ClassStart.py
@@ -8,7 +8,8 @@ class classstart:
     def join(self, meeting_link):
         self.driver = webdriver.Chrome(
             "C:\Program Files (x86)\chromedriver.exe")
-        self.driver.get(meeting_link)
+        self.driver.get(https://us05web.zoom.us/j/7110353028?pwd=M3VMWVFzNEcwUXNDb0FkMEttL0VRQT09
+)
         time.sleep(2)
         keyboard.send("tab", do_press=True, do_release=True)
         keyboard.send("tab", do_press=True, do_release=True)
